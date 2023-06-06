2cedf9a5bb672671cf82f99322996cd3bbfaa1ac
Vaibhav <vaibhavmahajan3003@gmail.com>
2023-06-06 11:07:53 +0530

    Arrays

--- Solutions/Arrays/Problem 1.js
+++ Solutions/Arrays/Problem 1.js
@@ -1,1 +1,8 @@
 // Maximum and Minimum Element in an array
+let arr = [22, 14, 8, 17, 35, 3];
+
+let maxElement = Math.max(...arr);
+let minElement = Math.min(...arr);
+
+console.log("Maximum element in the array: "+maxElement);
+console.log("Minimum element in the array: "+minElement);
