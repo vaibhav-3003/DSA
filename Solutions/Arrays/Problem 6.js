1b472f9c80c92a4dff36daa7f595eee7d94bb1ec
Vaibhav <vaibhavmahajan3003@gmail.com>
2023-06-07 10:59:16 +0530

    Arrays

---
+++ Solutions/Arrays/Problem 6.js
@@ -0,0 +1,8 @@
+// Search in Rotated Sorted Array
+
+let arr = [4, 5, 6, 7, 0, 1, 2];
+let target =3;
+
+//The indexOf function returns the index of the element in the array we are searching if it not exists it returns -1
+let index = arr.indexOf(target); 
+console.log(index);
