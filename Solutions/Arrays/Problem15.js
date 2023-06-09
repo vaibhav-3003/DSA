50bc5e37bed496fd91ad065fc6d3e9b83a64f74b
Vaibhav <vaibhavmahajan3003@gmail.com>
2023-06-09 12:22:11 +0530

    Arrays

--- Solutions/Arrays/Problem15.js
+++ Solutions/Arrays/Problem15.js
@@ -1,12 +1,32 @@
-let target = "c";
+//Longest subarray with sum divisible by K
 
-let letters = ["c", "f", "j"];
-let ans = "";
+function longestSubarrayWithSumDivisibleByK(arr, k) {
+  let maxLength = 0;
+  let prefixSum = 0;
+  let prefixSumMap = new Map();
 
-for(let i of letters){
-    if(i>target){
-        ans+=i;
-        break;
+  // Initialize the prefix sum map with the base case
+  prefixSumMap.set(0, -1);
+
+  for (let i = 0; i < arr.length; i++) {
+    prefixSum = (prefixSum + arr[i]) % k;
+
+    if (prefixSum < 0) {
+      prefixSum += k; // Ensure positive modulo value
     }
+
+    if (prefixSumMap.has(prefixSum)) {
+      maxLength = Math.max(maxLength, i - prefixSumMap.get(prefixSum));
+    } else {
+      prefixSumMap.set(prefixSum, i);
+    }
+  }
+
+  return maxLength;
 }
-console.log(ans);
+
+const arr = [7, 8, 10];
+const k = 3;
+
+const result = longestSubarrayWithSumDivisibleByK(arr, k);
+console.log(result);
