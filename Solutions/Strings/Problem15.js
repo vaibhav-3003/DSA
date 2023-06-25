504b426cf8c17af39890b5e5641603608fe46e4a
Vaibhav <vaibhavmahajan3003@gmail.com>
2023-06-25 18:18:29 +0530

    Two Pointers

---
+++ Solutions/Strings/Problem15.js
@@ -0,0 +1,15 @@
+//Wildcard String Matching
+// let wild = "ge*ks";
+// let pattern = "geeks";
+// let specialChars = ["!","@"]
+
+// for(let i=0;i<pattern.length;i++){
+    
+// }
+// console.log(j);
+N = 48
+if(N%5===0){
+    console.log(-1)
+}else{
+    console.log(N%5)
+}
